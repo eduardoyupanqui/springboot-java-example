91a48b6a29974f6ff7a312954e870c71f372430d
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-07-15 13:41:20 -0500

    Initial commit

---
+++ src/main/java/com/yup/demo1/Demo1Application.java
@@ -0,0 +1,16 @@
+package com.yup.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@SpringBootApplication
+public class Demo1Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Demo1Application.class);
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+		logger.info("Hola Spring Boot");
+	}
+
+}
