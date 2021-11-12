8cbf240f0f4cd2dfedfed990130eb2a7e1867d1b
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2021-11-12 01:35:53 -0500

    Add Some changes

--- src/main/java/com/yup/microservices/demo/Demo1Application.java
+++ src/main/java/com/yup/microservices/demo/Demo1Application.java
@@ -1,9 +1,12 @@
 package com.yup.microservices.demo;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class Demo1Application {
 
@@ -13,5 +16,4 @@ public class Demo1Application {
 		logger.info("Hola Spring Boot");
 		logger.info("Hola Spring Boot");
 	}
-
 }
---
+++ src/main/java/com/yup/microservices/demo/config/SleuthConfig.java
@@ -0,0 +1,41 @@
+package com.yup.microservices.demo.config;
+
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+
+import javax.servlet.Filter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.WebFilter;
+
+@Configuration(proxyBeanMethods = false)
+public class SleuthConfig {
+    // Example of a servlet Filter for non-reactive applications
+    @Bean
+    Filter traceIdInResponseFilter(Tracer tracer) {
+        return (request, response, chain) -> {
+            Span currentSpan = tracer.currentSpan();
+            if (currentSpan != null) {
+                HttpServletResponse resp = (HttpServletResponse) response;
+                // putting trace id value in [mytraceid] response header
+                resp.addHeader("request-id", currentSpan.context().traceId());
+            }
+            chain.doFilter(request, response);
+        };
+    }
+
+    // Example of a reactive WebFilter for reactive applications
+//    @Bean
+//    WebFilter traceIdInResponseFilter(Tracer tracer) {
+//        return (exchange, chain) -> {
+//            Span currentSpan = tracer.currentSpan();
+//            if (currentSpan != null) {
+//                // putting trace id value in [mytraceid] response header
+//                exchange.getResponse().getHeaders().add("mytraceid", currentSpan.context().traceId());
+//            }
+//            return chain.filter(exchange);
+//        };
+//    }
+}
